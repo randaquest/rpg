78934f05b16864449d2eccafd7bf434a7e2b4e5f
randaquest <randaquest@gmail.com>
2015-03-23 12:26:47 -0400

    Fixed bug in battle.html & upgraded populate_rpg.py to include items

--- rpg_project/populate_rpg.py
+++ rpg_project/populate_rpg.py
@@ -16,14 +16,29 @@ def populate():
     add_monster('newb2','picofnewb','0','10','False','10', Area.objects.get(name='tutorial island2'))
     add_monster('newb3','picofnewb','0','10','False','10', Area.objects.get(name='tutorial island3'))
     add_monster('newb4','picofnewb','0','10','False','10', Area.objects.get(name='tutorial island4'))
+    add_item('item of testing','picofitemoftest')
+    add_weapon('sword of testing','picofswordoftest','3','6')
+    add_armor('armor of testing','picofarmoroftest','10')
 
 def add_area(name, picture, rarity, backstory):
-    p = Area.objects.get_or_create(name=name, picture=picture, rarity=rarity, backstory=backstory)[0]
-    return p
+    a = Area.objects.get_or_create(name=name, picture=picture, rarity=rarity, backstory=backstory)[0]
+    return a
+
+def add_item(name, picture):
+    i = Item.objects.get_or_create(name=name, picture=picture)[0]
+    return i
+
+def add_weapon(name, picture, minD, maxD):
+    w = Weapon.objects.get_or_create(name=name, picture=picture, minD=minD, maxD=maxD)[0]
+    return w
+
+def add_armor(name, picture, defence):
+    ar = Armor.objects.get_or_create(name=name, picture=picture, defence=defence)[0]
+    return ar
 
 def add_monster(name, picture, rarity, maxHP, boss, baseXP, area):
-    c = Monster.objects.get_or_create(name=name, picture=picture, rarity=rarity, maxHP=maxHP, boss=boss, baseXP=baseXP, area=area)[0]
-    return c
+    m = Monster.objects.get_or_create(name=name, picture=picture, rarity=rarity, maxHP=maxHP, boss=boss, baseXP=baseXP, area=area)[0]
+    return m
 
 # Start execution here!
 if __name__ == '__main__':
